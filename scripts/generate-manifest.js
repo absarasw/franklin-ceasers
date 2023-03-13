50582e69911347efc3631bb79dd1909f5ea5b004
Abhinav Saraswat <abhinavsaraswat@Abhinavs-MacBook-Pro-6.local>
2023-03-14 00:13:56 +0530

    Updating screens-manifest-generator scope

--- scripts/generate-manifest.js
+++ scripts/generate-manifest.js
@@ -1,4 +1,4 @@
-import generateManifest from '@adobe/screens-manifest-generator';
+import generateManifest from '@aem-screens/screens-manifest-generator';
 
 /* function temp() {
     const generateManifest = require('@adobe/screens-manifest-generator/src/generateManinfest.js')
